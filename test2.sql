6f43f9c40cfb9704bdc2bfef569a7460696de357
duxphuc20703 <ducphuc20703@gmail.com>
2023-02-23 21:14:38 +0700

    first commit

---
+++ test2.sql
@@ -0,0 +1,464 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 22, 2023 lúc 02:46 AM
+-- Phiên bản máy phục vụ: 10.4.27-MariaDB
+-- Phiên bản PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `test2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `classes`
+--
+
+CREATE TABLE `classes` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `classes`
+--
+
+INSERT INTO `classes` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'IT0501', '2023-03-19 08:32:17', '2023-03-19 08:32:17'),
+(2, 'IT0502', '2023-03-19 08:32:28', '2023-03-19 08:32:28');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(253, '2014_10_12_000000_create_users_table', 1),
+(254, '2014_10_12_100000_create_password_resets_table', 1),
+(255, '2019_08_19_000000_create_failed_jobs_table', 1),
+(256, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(257, '2022_01_13_015525_create_subjects_table', 1),
+(258, '2022_01_13_015542_create_teacher_profiles_table', 1),
+(259, '2022_01_13_020203_create_classes_table', 1),
+(260, '2022_01_13_020204_create_student_profiles_table', 1),
+(261, '2022_01_13_090426_create_teacher_subject_table', 1),
+(262, '2022_01_13_100112_create_scores_table', 1),
+(263, '2022_01_14_143526_create_request_edit_score_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `request_edit_score`
+--
+
+CREATE TABLE `request_edit_score` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `score_id` bigint(20) UNSIGNED NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `scores`
+--
+
+CREATE TABLE `scores` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `student_id` bigint(20) UNSIGNED NOT NULL,
+  `subject_id` bigint(20) UNSIGNED NOT NULL,
+  `tp1` double(8,2) UNSIGNED NOT NULL,
+  `tp2` double(8,2) UNSIGNED DEFAULT NULL,
+  `qt` double(8,2) UNSIGNED DEFAULT NULL,
+  `ck` double(8,2) UNSIGNED DEFAULT NULL,
+  `tk` double(8,2) UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `scores`
+--
+
+INSERT INTO `scores` (`id`, `student_id`, `subject_id`, `tp1`, `tp2`, `qt`, `ck`, `tk`, `created_at`, `updated_at`) VALUES
+(2, 2, 1, 90.00, 90.00, NULL, NULL, 90.00, '2023-03-21 18:33:34', '2023-03-21 18:33:34'),
+(3, 6, 1, 70.00, 70.00, NULL, NULL, 70.00, '2023-03-21 18:43:51', '2023-03-21 18:43:51'),
+(4, 3, 1, 75.00, 75.00, NULL, NULL, 75.00, '2023-03-21 18:44:49', '2023-03-21 18:44:49');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `student_profiles`
+--
+
+CREATE TABLE `student_profiles` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `dob` datetime NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `class_id` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`class_id`)),
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `student_profiles`
+--
+
+INSERT INTO `student_profiles` (`id`, `dob`, `code`, `class_id`, `created_at`, `updated_at`) VALUES
+(1, '2023-03-19 00:00:00', 'BH00113', '[\"1\",\"2\"]', '2023-03-19 08:32:49', '2023-03-19 08:32:49'),
+(2, '2023-03-22 00:00:00', 'BH00114', '[\"1\",\"2\"]', '2023-03-21 18:19:15', '2023-03-21 18:19:15'),
+(3, '2023-03-22 00:00:00', 'BH0000', '[\"2\"]', '2023-03-21 18:23:31', '2023-03-21 18:44:33'),
+(6, '2023-03-22 00:00:00', 'BH000000', '[\"1\",\"2\"]', '2023-03-21 18:43:22', '2023-03-21 18:43:22');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `semester` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `name`, `code`, `semester`, `created_at`, `updated_at`) VALUES
+(1, 'Semi-Prject', 'SP', 4, '2023-03-21 18:21:02', '2023-03-21 18:21:02');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `teacher_profiles`
+--
+
+CREATE TABLE `teacher_profiles` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `teacher_profiles`
+--
+
+INSERT INTO `teacher_profiles` (`id`, `created_at`, `updated_at`) VALUES
+(1, '2023-03-19 08:31:54', '2023-03-19 08:31:54');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `teacher_subject`
+--
+
+CREATE TABLE `teacher_subject` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `teacher_id` bigint(20) UNSIGNED NOT NULL,
+  `subject_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `teacher_subject`
+--
+
+INSERT INTO `teacher_subject` (`id`, `teacher_id`, `subject_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '2023-03-21 18:21:02', '2023-03-21 18:21:02'),
+(2, 1, 1, '2023-03-21 18:21:02', '2023-03-21 18:21:02');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `profile_id` bigint(20) UNSIGNED NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `username`, `role`, `profile_id`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Ngo Thi Mai Loan', 'teacher@gmail.com', 'teacher', 'teacher', 1, '$2y$10$tXCZlbmQwLn.3CCtohuY.OrhthY.nB0RjE82WsvCb8Fnw.6IXnnFi', 'HnGyFhiglylMzNROTS0zo1HL7Zs5xrLsVdVg3MTIsQLKSmB2G3zPb4snMIhx', NULL, NULL),
+(2, 'Le Van Thuan', 'Thuanlv@gmail.com', 'ThuanLV', 'teacher', 1, '$2y$10$ai8mTn1xAFOCFxQzlWqBVuil9mr5FDfsrFFoIZmbSBqNII1Nsf88.', NULL, '2023-03-19 08:31:54', '2023-03-19 08:31:54'),
+(3, 'Tran Duc Phuc', 'phuctdbh00113@fpt.edu.vn', 'BH00113', 'student', 1, '$2y$10$Y9q6S7vZ9ZVmnStMKQTFb.Hkf8eiPWu4yuZJhkKm7RGsF43MHZTQS', NULL, '2023-03-19 08:32:49', '2023-03-19 08:32:49'),
+(4, 'Hoang Thu Thuy', 'Thuyth@fpt.edu.vn', 'BH00114', 'student', 2, '$2y$10$478vwm8ul/O33c8yZyFvbeZPK4NQTDAhcSXrloQZoGgbBc8vA/BPi', NULL, '2023-03-21 18:19:15', '2023-03-21 18:19:15'),
+(5, 'Tran Thanh DO', 'ducphuc20703@gmail.com', 'BH0000', 'student', 3, '$2y$10$BcYyaeAW.kl5HVGdKkz0M.OPu35YJaemc51swJMzOINKh6wSAEipG', NULL, '2023-03-21 18:23:31', '2023-03-21 18:23:31'),
+(8, 'Nguyen Phi Hung', 'hungnt@fpt.edu.vn', 'BH000000', 'student', 6, '$2y$10$0NafwR9MQ9aw.JM.1TjHwO/OwRwCEfDvakbR65c000ocOxGLeN/iK', NULL, '2023-03-21 18:43:22', '2023-03-21 18:43:22');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Chỉ mục cho bảng `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Chỉ mục cho bảng `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Chỉ mục cho bảng `request_edit_score`
+--
+ALTER TABLE `request_edit_score`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `request_edit_score_score_id_foreign` (`score_id`);
+
+--
+-- Chỉ mục cho bảng `scores`
+--
+ALTER TABLE `scores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `scores_student_id_foreign` (`student_id`),
+  ADD KEY `scores_subject_id_foreign` (`subject_id`);
+
+--
+-- Chỉ mục cho bảng `student_profiles`
+--
+ALTER TABLE `student_profiles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `teacher_profiles`
+--
+ALTER TABLE `teacher_profiles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `teacher_subject`
+--
+ALTER TABLE `teacher_subject`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `teacher_subject_teacher_id_foreign` (`teacher_id`),
+  ADD KEY `teacher_subject_subject_id_foreign` (`subject_id`);
+
+--
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD UNIQUE KEY `users_username_unique` (`username`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT cho bảng `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=264;
+
+--
+-- AUTO_INCREMENT cho bảng `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `request_edit_score`
+--
+ALTER TABLE `request_edit_score`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `scores`
+--
+ALTER TABLE `scores`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT cho bảng `student_profiles`
+--
+ALTER TABLE `student_profiles`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT cho bảng `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `teacher_profiles`
+--
+ALTER TABLE `teacher_profiles`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `teacher_subject`
+--
+ALTER TABLE `teacher_subject`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `request_edit_score`
+--
+ALTER TABLE `request_edit_score`
+  ADD CONSTRAINT `request_edit_score_score_id_foreign` FOREIGN KEY (`score_id`) REFERENCES `scores` (`id`) ON DELETE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `scores`
+--
+ALTER TABLE `scores`
+  ADD CONSTRAINT `scores_student_id_foreign` FOREIGN KEY (`student_id`) REFERENCES `student_profiles` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `scores_subject_id_foreign` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`) ON DELETE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `teacher_subject`
+--
+ALTER TABLE `teacher_subject`
+  ADD CONSTRAINT `teacher_subject_subject_id_foreign` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`),
+  ADD CONSTRAINT `teacher_subject_teacher_id_foreign` FOREIGN KEY (`teacher_id`) REFERENCES `teacher_profiles` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
