fdaa2788b2a1620c658f376f1ef607a4fff460d9
duxphuc20703 <ducphuc20703@gmail.com>
2023-03-06 22:07:48 +0700

    socres/semester

--- resources/views/scores/semester/list.blade.php
+++ resources/views/scores/semester/list.blade.php
@@ -8,6 +8,7 @@
                 <thead>
                     <tr>
                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Semester</th>
+                        
                         <th class="text-secondary opacity-7"></th>
                     </tr>
                 </thead>
