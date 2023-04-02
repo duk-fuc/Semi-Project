258db6827c2f299d941f43f1f04f76883ba82bcb
duxphuc20703 <ducphuc20703@gmail.com>
2023-04-02 08:36:21 +0700

    views/classes/form

--- resources/views/classes/form.blade.php
+++ resources/views/classes/form.blade.php
@@ -5,9 +5,11 @@
     action="{{isset($rec) ? route('classes.update', ['id' => $rec->id]) : route('classes.create')}}">
     {{ csrf_field() }}
     <label class="form-label mt-3">Class name *</label>
+    
     <div class="input-group input-group-outline">
         <input type="text" name="name" class="form-control" required value="{{$rec->name ?? old('name') ?? ''}}">
     </div>
+
     <input type="submit" class="btn bg-gradient-primary my-4 mb-2" value="{{ isset($rec) ? 'Update' : 'Add'}}">
 </form>
 @stop
